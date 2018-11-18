0986c5426bbf0f20d0fc26556a587a35e42e79cf
Katz, Matan <matan.katz@intel.com>
2018-11-18 20:40:46 +0200

    WIP

--- examples/rs_viewer.hpp
+++ examples/rs_viewer.hpp
@@ -51,7 +51,7 @@ public:
         _window_title(window_title),
         processing_block([this](rs2::frame f, rs2::frame_source& s)
     {
-        f.keep();
+        //f.keep();
         _input_queue.enqueue(f);
         s.frame_ready(f);
     })
@@ -59,68 +59,106 @@ public:
         _render_thread = std::thread(&viewer_window::render_thread, this);
     }
 
+    ~viewer_window()
+    {
+        if(_render_thread.joinable())
+            _render_thread.join();
+        _last_frames.clear();
+        glfwTerminate();
+    }
+
     operator bool()
     {
-        return !glfwWindowShouldClose(_window);
+        return !glfwWindowShouldClose(_window.get());
     }
 
 private:
     int _window_width;
     int _window_height;
     std::string _window_title;
-    GLFWwindow* _window;
 
-    //std::shared_ptr<GLFWwindow> _window;
+    std::shared_ptr<GLFWwindow> _window;
+    GLuint _frame_buffer_name = 0;
 
     rs2::frame_queue _input_queue;
-
     std::thread _render_thread;
 
     std::map<std::pair<rs2_stream, int>, rect> _view_ports;
     std::map<std::pair<rs2_stream, int>, float2> _frame_size;
     std::map<std::pair<rs2_stream, int>, rs2::frame> _last_frames;
 
-    //glfw_state _glfw_state;
-    GLuint _frame_buffer_name = 0;
+    std::shared_ptr<GLFWwindow> create_gl_window()
+    {
+        glfwInit();
+        auto w = glfwCreateWindow(_window_width, _window_height, _window_title.c_str(), nullptr, nullptr);
+        if (!w)
+            throw std::runtime_error("Could not open OpenGL window, please check your graphic drivers or use the textual SDK tools");
+        return std::shared_ptr<GLFWwindow>(w, [](GLFWwindow * w) 
+        {
+            glfwDestroyWindow(w); 
+        });
+    }
+
+    bool try_add_frame(const rs2::frame& f)
+    {
+        if (!can_render(f)) 
+            return false;
+        auto p = f.get_profile();
+        _last_frames[{p.stream_type(), p.stream_index()}] = f;
+        update_viewports_grid(f);
+        return true;
+    }
+
+    bool try_handle_frame(const rs2::frame& f)
+    {
+        if (!f)
+            return false;
+        bool rv = false;
+        if (f.is<rs2::frameset>())
+        {
+            auto set = f.as<rs2::frameset>();
+            set.foreach([&](rs2::frame f)
+            {
+                if (try_add_frame(f))
+                    rv = true;
+            });
+        }
+        else
+            return try_add_frame(f);
+        return rv;
+    }
 
     void render_thread()
     {
-        glfwInit();
-        _window = glfwCreateWindow(_window_width, _window_height, _window_title.c_str(), nullptr, nullptr);
-        if (!_window)
-            throw std::runtime_error("Could not open OpenGL window, please check your graphic drivers or use the textual SDK tools");
+        _window = create_gl_window();
         glGenTextures(1, &_frame_buffer_name);
 
-        //_window = std::make_shared<GLFWwindow>();
-        //register_glfw_callbacks(*_window.get(), _glfw_state);
-
-        while (true)
+        while (!glfwWindowShouldClose(_window.get()))
         {
             auto f = _input_queue.wait_for_frame();
-            if (!f)
-                continue;
-            auto p = f.get_profile();
-            _last_frames[{p.stream_type(), p.stream_index()}] = f;
 
-            glfwMakeContextCurrent(_window);
+            try_handle_frame(f);
+
+            glfwMakeContextCurrent(_window.get());
             glfwPollEvents();
 
-            update_viewports_grid(f);
-
-            for (auto r : _last_frames)
+            for (auto& r : _last_frames)
+            {
                 render(r.second);
+            }
 
-            glfwSwapBuffers(_window);
+            glfwSwapBuffers(_window.get());
             glFlush();
         }
     }
 
-    void update_viewports_grid(rs2::video_frame f)
+    void update_viewports_grid(const rs2::frame& f)
     {
         auto p = f.get_profile();
         auto it = _frame_size.find({ p.stream_type(), p.stream_index() });
+        auto vf = f.as<rs2::video_frame>();
         if (it == _frame_size.end())
-            _frame_size[{p.stream_type(), p.stream_index()}] = { (float)f.get_width(), (float)f.get_height() };
+            _frame_size[{p.stream_type(), p.stream_index()}] = { (float)vf.get_width(), (float)vf.get_height() };
 
         calc_grid();
 
@@ -128,10 +166,11 @@ private:
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     }
 
-    void render(rs2::video_frame f)
+    void render(const rs2::frame& f)
     {
         auto p = f.get_profile();
         auto r = _view_ports[{p.stream_type(), p.stream_index()}];
+        auto& vf = f.as<rs2::video_frame>();
 
         glViewport(r.x, r.y, r.w, r.h);
 
@@ -144,13 +183,13 @@ private:
         switch (format)
         {
         case RS2_FORMAT_RGB8:
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, f.get_width(), f.get_height(), 0, GL_RGB, GL_UNSIGNED_BYTE, f.get_data());
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, vf.get_width(), vf.get_height(), 0, GL_RGB, GL_UNSIGNED_BYTE, f.get_data());
             break;
         case RS2_FORMAT_RGBA8:
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, f.get_width(), f.get_height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, f.get_data());
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, vf.get_width(), vf.get_height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, f.get_data());
             break;
         case RS2_FORMAT_Y8:
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, f.get_width(), f.get_height(), 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, f.get_data());
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, vf.get_width(), vf.get_height(), 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, f.get_data());
             break;
         default:
             throw std::runtime_error("The requested format is not supported by this demo!");
@@ -193,7 +232,7 @@ private:
 
     void calc_grid()
     {
-        glfwGetWindowSize(_window, &_window_width, &_window_height);
+        glfwGetWindowSize(_window.get(), &_window_width, &_window_height);
         auto grid = calc_cell_size({(float)_window_width, (float)_window_height}, _frame_size.size());
 
         int index = 0;
@@ -211,6 +250,20 @@ private:
             auto r = rect{ cell_x_postion, cell_y_position, grid.w, grid.h };
             _view_ports[f.first] = r.adjust_ratio(float2{ fw, fh });
             index++;
+        }
+    }
+
+    bool can_render(const rs2::frame& f) const
+    {
+        auto format = f.get_profile().format();
+        switch (format)
+        {
+        case RS2_FORMAT_RGB8:
+        case RS2_FORMAT_RGBA8:
+        case RS2_FORMAT_Y8:
+            return true;
+        default:
+            return false;
         }
     }
 };
